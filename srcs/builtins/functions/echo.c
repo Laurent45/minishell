7c71268d69850dba7c49213a164ed982e61af15a
Esquanor_linux <outsider973@outlook.fr>
2022-09-08 16:31:25 +0200

    update echo builtin

--- srcs/builtins/functions/echo.c
+++ srcs/builtins/functions/echo.c
@@ -6,28 +6,48 @@
 /*   By: lfrederi <lfrederi@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/30 22:38:42 by lfrederi          #+#    #+#             */
-/*   Updated: 2022/09/07 22:29:21 by lfrederi         ###   ########.fr       */
+/*   Updated: 2022/09/08 16:29:39 by lfrederi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "builtins.h"
-#include "error.h"
-#include "expansion.h"
-#include "ft_string.h"
 #include "ft_stdio.h"
+#include "init.h"
+
+static int	check_newline(t_list **args)
+{
+	int		newline;
+	char	*args_str;
+	int		i;
+
+	newline = 1;
+	while (*args)
+	{
+		*args = (*args)->next;
+		args_str = (char *) (*args)->content;
+		i = 0;
+		if (args_str[i] == '-')
+		{
+			i++;
+			while (args_str[i] == 'n')
+				i++;
+			if (args_str[i] == '\0')
+				newline = 0;
+			else
+				break ;
+		}
+		else
+			break ;
+	}
+	return (newline);
+}
 
 int	built_echo(t_list *args, t_list **my_envp)
 {
 	(void) my_envp;
 	int		newline;
 
-	newline = 1;
-	if (args->next && ft_strcmp((char *) args->next->content, "-n") == 0)
-	{
-		newline = 0;
-		args = args->next;
-	}
-	args = args->next;
+	newline = check_newline(&args);
 	while (args)
 	{
 		ft_putstr_fd((char *) args->content, 1);
@@ -36,5 +56,5 @@ int	built_echo(t_list *args, t_list **my_envp)
 	}
 	if (newline)
 		ft_putstr_fd("\n", 1);
-	return (0);
+	return (set_status(0), SUCCESS);
 }
